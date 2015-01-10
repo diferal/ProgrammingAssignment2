3b8d91c1add8fa5a640a8bf159f7174803b2e1fe
diferal <nmanyak1@WPRDBER9ZRH2R.wks.jnj.com>
2015-01-10 20:41:53 +0100

    Some grammatical mistakes in comments were corrected

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,5 @@
-## Matrix inversion is usually a costly computation and their may be some benefit to caching the inverse 
-## of a matrix rather than compute it repeatedly. Folling funtions allows to:
+## Matrix inversion is usually a costly computation and there may be some benefit to caching the inverse 
+## of a matrix rather than compute it repeatedly. Folling functions allows to:
 ## 1. makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
 ## 2. cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
 ## It is assume that the matrices supplied are square, numerical and always invertible
